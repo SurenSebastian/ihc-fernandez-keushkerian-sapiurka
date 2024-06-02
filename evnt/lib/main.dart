760be6ce41ff084a658849953bcab65905252c09
Mateo Sapiurka <mateo.sapiurka@gmail.com>
2024-06-02 16:29:56 -0300

    Implementing Firestore Integration [WIP]

--- evnt/lib/main.dart
+++ evnt/lib/main.dart
@@ -1,19 +1,50 @@
+import 'package:evnt/screens/event_screen.dart';
+import 'package:evnt/screens/event_search.dart';
+import 'package:evnt/screens/my_events_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/start_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'E-vnt',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home: StartScreen(),
-    );
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          // Error screen
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'E-vnt',
+            theme: ThemeData(
+              primarySwatch: Colors.orange,
+            ),
+            home: const EventSearchScreen(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return const Center(child: CircularProgressIndicator());
+      }
+    );    
   }
 }
--- evnt/lib/widgets/event_detailed_card.dart
+++ evnt/lib/widgets/event_detailed_card.dart
@@ -1,25 +1,25 @@
+import 'package:evnt/services/models.dart';
 import 'package:flutter/material.dart';
 
 class EventDetailedCard extends StatelessWidget {
-  final Map<String, String> event;
-
-  EventDetailedCard({required this.event});
+  final Event event;
+  const EventDetailedCard({super.key, required this.event});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16.0),
-        side: BorderSide(color: Colors.orange),
+        side: const BorderSide(color: Colors.orange),
       ),
       child: ListTile(
-        leading: Icon(
+        leading: const Icon(
           Icons.image,
           color: Colors.orange,
         ),
-        title: Text(event['title']!),
-        subtitle: Text('${event['location']} ${event['time']}'),
+        title: Text(event.title),
+        subtitle: Text('${event.location} ${event.time}'),
       ),
     );
   }
