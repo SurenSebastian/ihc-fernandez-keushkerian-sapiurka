7edd06057155c6073f059367dcb0a973c9dd2508
Mateo Sapiurka <mateo.sapiurka@gmail.com>
2024-06-15 18:04:09 -0300

    extended event domain

--- evnt/lib/services/models.dart
+++ evnt/lib/services/models.dart
@@ -1,17 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:json_annotation/json_annotation.dart';
 part 'models.g.dart';
 
 @JsonSerializable()
 class Event {
   final String title;
+  final String descripcion;
+  final DateTime dateTime;
   final String location;
-  final String time;
+  final double price;
+  final List<String> tags;
+  final String owner;
 
   Event({
       this.title = '',
+      this.descripcion = '',
+      required this.dateTime,
       this.location = '',
-      this.time = '',
-    });
+      this.price = 0,
+      this.tags = const [],
+      this.owner = '',
+  });
 
   factory Event.fromJson(Map<String, dynamic> json) => _$EventFromJson(json);
   Map<String, dynamic> toJson() => _$EventToJson(this);
--- evnt/lib/services/models.g.dart
+++ evnt/lib/services/models.g.dart
@@ -8,12 +8,22 @@ part of 'models.dart';
 
 Event _$EventFromJson(Map<String, dynamic> json) => Event(
       title: json['title'] as String? ?? '',
+      descripcion: json['descripcion'] as String? ?? '',
+      dateTime: (json['dateTime'] as Timestamp).toDate(),
       location: json['location'] as String? ?? '',
-      time: json['time'] as String? ?? '',
+      price: (json['price'] as num?)?.toDouble() ?? 0,
+      tags:
+          (json['tags'] as List<dynamic>?)?.map((e) => e as String).toList() ??
+              const [],
+      owner: json['owner'] as String? ?? '',
     );
 
 Map<String, dynamic> _$EventToJson(Event instance) => <String, dynamic>{
       'title': instance.title,
+      'descripcion': instance.descripcion,
+      'dateTime': instance.dateTime.toIso8601String(),
       'location': instance.location,
-      'time': instance.time,
+      'price': instance.price,
+      'tags': instance.tags,
+      'owner': instance.owner,
     };
