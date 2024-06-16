f1a7d116fcbe60ba8e60bc877419feff742772e4
Suren Keushkerian <surensebastian@gmail.com>
2024-06-16 18:18:25 -0300

    Se agregan validaciones al create_account y se deja look and feel similar

--- evnt/lib/widgets/date_dropdown.dart
+++ evnt/lib/widgets/date_dropdown.dart
@@ -24,11 +24,8 @@ class DateDropdown extends StatelessWidget {
       icon: const Icon(Icons.arrow_drop_down),
       iconSize: 24,
       elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
+      style: const TextStyle(color: Colors.black),
+      underline: Container(height: 2, color: Colors.deepOrange[50]),
       onChanged: onChanged,
       items: items.map<DropdownMenuItem<String>>((String value) {
         return DropdownMenuItem<String>(
