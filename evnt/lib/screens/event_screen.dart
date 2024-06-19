c4d7e26fda4713255cb49104369dacdb2894b94f
Mateo Sapiurka <mateo.sapiurka@gmail.com>
2024-06-19 20:04:56 -0300

    Navigation to event full info

--- evnt/lib/screens/event_screen.dart
+++ evnt/lib/screens/event_screen.dart
@@ -1,14 +1,16 @@
-import 'package:evnt/widgets/custom_button.dart';
+import 'package:evnt/services/models.dart';
 import 'package:flutter/material.dart';
 
 class EventScreen extends StatelessWidget {
-  const EventScreen({super.key});
+  final Event event;
+
+  const EventScreen({super.key, required this.event});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Rock Concert'),
+        title: Text(event.title),
         backgroundColor: Colors.orange,
       ),
       body: Padding(
@@ -16,17 +18,17 @@ class EventScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Center(
+            Center(
               child: Column(
                 children: [
-                  Icon(
-                    Icons.emoji_events,
+                  const Icon(
+                    Icons.event,
                     color: Colors.orange,
                     size: 100,
                   ),
                   Text(
-                    'Rock Concert',
-                    style: TextStyle(
+                    event.title,
+                    style: const TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                       color: Colors.orange,
@@ -36,64 +38,24 @@ class EventScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 16),
-            const Text(
-              'An event created for the ones who love rock and would like to have a nice evening. Bands playing would be Maneskin, St Louis and much more!',
-              style: TextStyle(fontSize: 16),
+            Text(
+              event.description,
+              style: const TextStyle(fontSize: 16),
             ),
             const SizedBox(height: 16),
-            const Text(
-              'Day: 18th March',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            Text(
+              'Day: ${event.dateTime.day}/${event.dateTime.month}/${event.dateTime.year}',
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-            const Text(
-              'Hour: 13:00 PM - 21:00 PM',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            Text(
+              'Hour: ${event.dateTime.hour}:${event.dateTime.minute}',
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-            const Text(
-              'Tags: food, rock, outdoor',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            const Center(
-              child: Column(
-                children: [
-                  Text(
-                    '120',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    'have confirmed their attendance',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
+            Text(
+              'Tags: ${event.tags.join(', ')}',
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
             const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CustomButton(
-                  text: 'Save for Later',
-                  color: Colors.white,
-                  textColor: Colors.orange,
-                  onPressed: () {
-                    // Save for later action
-                  }
-                ),
-                CustomButton(
-                  text: 'Attend',
-                  color: Colors.orange,
-                  textColor: Colors.white,
-                  onPressed: () {
-                    // Attend action
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
           ],
         ),
       ),
--- evnt/lib/widgets/event_card.dart
+++ evnt/lib/widgets/event_card.dart
@@ -1,4 +1,5 @@
 import 'package:evnt/services/models.dart';
+import 'package:evnt/screens/event_screen.dart';
 import 'package:flutter/material.dart';
 
 class EventCard extends StatelessWidget {
@@ -8,18 +9,28 @@ class EventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.0),
-        side: const BorderSide(color: Colors.orange),
-      ),
-      child: ListTile(
-        leading: const Icon(
-          Icons.image,
-          color: Colors.orange,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => EventScreen(event: event),
+          ),
+        );
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+          side: const BorderSide(color: Colors.orange),
         ),
-        title: Text(event.title),
+        child: ListTile(
+          leading: const Icon(
+            Icons.event,
+            color: Colors.orange,
+          ),
+          title: Text(event.title),
+        ),
       ),
     );
   }
